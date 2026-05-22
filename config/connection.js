f247fb0d0bb301e1d7cca0a82af32c3324d96002
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-22 15:53:16 -0400

    add mongoDB connection utility with error handling using mongoose

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,13 @@
 //DB Connection
 const mongoose = require('mongoose');
 
-async function mongoConnect(){
+async function mongoConnect() {
     try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB Connection Successful');
 
     } catch (error) {
-
+        console.error('Server Error: ', error.message);
+        process.exit(1); // exit code: Startup failed. Shut app down.
     }
 }
