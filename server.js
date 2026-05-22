df6341ec9a55d1008b820826d5a3e678c5b4edea
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-22 15:26:07 -0400

    set dns for mongoDB atlas configs

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
+//Setting Dns for MongoDB Atla configs
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 
 //MIDDLEWARE---------------------------------------------------------------------------
